9adfcc4f3e2371c20f72cdac910ab41a0f669607
Ishita <iishita_be22@thapar.edu>
2023-02-26 05:10:23 +0530

    initial commit

--- lib/HomePage/theme.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-// Colors that we use in our app
-const kPrimaryColor = Color.fromARGB(255, 23, 138, 100);
-const kTextColor = Color(0xFF3C4046);
-const kBackgroundColor = Color(0xFFF9F8FD);
